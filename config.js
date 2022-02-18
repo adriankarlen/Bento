9a73440ac5ef92bdad4b94c60ad0b00a78202eab
Adrian <18417539+adriankarlen@users.noreply.github.com>
2022-02-18 15:11:33 +0100

    Colors

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
   // Weather
   weatherKey: '39a8b902fc186b78b27fc089eec5508c', // Write here your API Key
-  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
